573b833d963a0e109d8b60f17a49cbbb18964a9c
KneeArrow <trj598@gmail.com>
2016-09-06 16:22:21 +0800

    Update onekeyss.sh

--- onekeyss.sh
+++ onekeyss.sh
@@ -4,8 +4,8 @@ wget --no-check-certificate https://raw.githubusercontent.com/KneeArrow/shadowso
 wget --no-check-certificate https://raw.githubusercontent.com/KneeArrow/net-speeder/master/net-speeder_CentOS.sh
 chmod +x shadowsocks.sh
 chmod +x shadowsocks-crond.sh
-chmod +x net_speeder_lazyinstall.sh
-sh net_speeder_lazyinstall.sh
+chmod +x net-speeder_CentOS.sh
+sh net-speeder_CentOS.sh
 echo '*/5 * * * * root bash /root/shadowsocks-crond.sh' >> /etc/init.d/crond
 echo 'nohup /usr/local/net_speeder/net_speeder venet0 "ip" >/dev/null 2>&1 & ' >> /etc/rc.local
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
