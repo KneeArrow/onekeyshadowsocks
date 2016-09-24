19764bb1a82ad8e45b1c7e836aa1acefda564027
KneeArrow <trj598@gmail.com>
2016-09-24 13:01:32 +0800

    Update onekeyss.sh

--- onekeyss.sh
+++ onekeyss.sh
@@ -7,9 +7,9 @@ chmod +x shadowsocks-crond.sh
 chmod +x net-speeder_CentOS.sh
 ./shadowsocks.sh 2>&1 | tee shadowsocks.log
 sh net-speeder_CentOS.sh
-echo '*/5 * * * * root bash /root/shadowsocks-crond.sh' >> /etc/init.d/crond
+echo '*/5 * * * * root bash /root/shadowsocks-crond.sh' >>  /etc/crontab
 echo 'nohup /usr/local/net_speeder/net_speeder venet0 "ip" >/dev/null 2>&1 & ' >> /etc/rc.local
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-echo '0 5 * * * reboot' >> /etc/init.d/crond  
+echo '0 5 * * * reboot' >>  /etc/crontab  
 reboot
 
