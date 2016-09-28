bc62000a3f44fbe4e92b6fa66717864f4cfd99d0
KneeArrow <trj598@gmail.com>
2016-09-28 23:35:43 +0800

    Update onekeyss.sh

--- onekeyss.sh
+++ onekeyss.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 wget --no-check-certificate https://raw.githubusercontent.com/KneeArrow/shadowsocks_install/master/shadowsocks.sh
 wget --no-check-certificate https://raw.githubusercontent.com/KneeArrow/shadowsocks_install/master/shadowsocks-crond.sh
-wget --no-check-certificate https://raw.githubusercontent.com/KneeArrow/net-speeder/master/net-speeder_CentOS.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/91yun/serverspeeder/master/serverspeeder-all.sh
 chmod +x shadowsocks.sh
 chmod +x shadowsocks-crond.sh
-chmod +x net-speeder_CentOS.sh
+chmod +x serverspeeder-all.sh
 ./shadowsocks.sh 2>&1 | tee shadowsocks.log
-sh net-speeder_CentOS.sh
+bash serverspeeder-all.sh
 echo '*/5 * * * * root bash /root/shadowsocks-crond.sh' >>  /etc/crontab
-echo 'nohup /usr/local/net_speeder/net_speeder venet0 "ip" >/dev/null 2>&1 & ' >> /etc/rc.local
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo '0 5 * * * reboot' >>  /etc/crontab  
 reboot
