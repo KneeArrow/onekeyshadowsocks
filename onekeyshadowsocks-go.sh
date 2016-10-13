0a949643c2963013a5a541271e3d02b24472d747
KneeArrow <trj598@gmail.com>
2016-10-13 14:44:04 +0800

    Update onekeyshadowsocks-go.sh

--- onekeyshadowsocks-go.sh
+++ onekeyshadowsocks-go.sh
@@ -14,6 +14,6 @@ echo '*/5 * * * * root bash /root/shadowsocks-crond.sh' >>  /etc/crontab
 #echo 'nohup /usr/local/net_speeder/net_speeder venet0 "ip" >/dev/null 2>&1 & ' >> /etc/rc.local
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo '0 5 * * * reboot' >>  /etc/crontab  
-#reboot
+reboot
 
 
