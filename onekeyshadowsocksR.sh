5cb4e62258eb5d41581cf154afbdd443d3c8b223
KneeArrow <trj598@gmail.com>
2016-12-16 16:11:41 +0800

    Update onekeyshadowsocksR.sh

--- onekeyshadowsocksR.sh
+++ onekeyshadowsocksR.sh
@@ -2,7 +2,7 @@
 wget --no-check-certificate https://raw.githubusercontent.com/KneeArrow/shadowsocks_install/master/shadowsocksR.sh
 wget --no-check-certificate https://raw.githubusercontent.com/KneeArrow/shadowsocks_install/master/shadowsocks-crond.sh
 wget -N --no-check-certificate https://raw.githubusercontent.com/KneeArrow/serverspeeder/master/serverspeeder-all.sh
-chmod +x shadowsocks-go.sh
+chmod +x shadowsocksR.sh
 chmod +x shadowsocks-crond.sh
 chmod +x serverspeeder-all.sh
 ./shadowsocks-go.sh 2>&1 | tee shadowsocks-go.log
